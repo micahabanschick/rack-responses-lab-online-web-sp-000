751631dbda0dbfb5762d2b15c622750c1f6ba8b4
Micah Banschick <micha.ban@gmail.com>
2020-05-01 21:13:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    time = Time.now.strftime("%k:%M")
+    time = Time.now.strftime("%k").to_i
     
     if time < 12
       resp.write "Good Morning!"
